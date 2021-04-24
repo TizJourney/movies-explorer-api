4d54f499dd17f2cc447989df6751793d8205a541
Konstantin Bondar <kostya.bondar@gmail.com>
2021-04-24 15:34:02 +0300

    #5: добавить схему для модели пользователя

---
+++ models/user.js
@@ -0,0 +1,56 @@
+const mongoose = require('mongoose');
+const bcrypt = require('bcryptjs');
+
+const emailValidator = require('validator');
+
+const errorTypes = require('../utils/errors');
+
+const userSchema = new mongoose.Schema({
+  email: {
+    type: String,
+    required: [true, 'Поле email является обязательным'],
+    unique: true,
+    validate: {
+      validator(v) {
+        return emailValidator.isEmail(v);
+      },
+      message: 'Неверный формат поле email',
+    },
+  },
+  password: {
+    type: String,
+    required: [true, 'Поле password является обязательным'],
+    minlength: 8,
+    select: false,
+  },
+  name: {
+    type: String,
+    required: false,
+    default: 'Жак-Ив Кусто',
+    minlength: 2,
+    maxlength: 30,
+  },
+},
+{
+  versionKey: false,
+});
+
+userSchema.statics.findUserByCredentials = function findUserByCredentials(email, password) {
+  return this.findOne({ email }).select('+password')
+    .then((user) => {
+      if (!user) {
+        return Promise.reject(new errorTypes.LoginError('Неправильные почта или пароль'));
+      }
+
+      return bcrypt.compare(password, user.password)
+        .then((matched) => {
+          if (!matched) {
+            return Promise.reject(new errorTypes.LoginError('Неправильные почта или пароль'));
+          }
+
+          return user;
+        });
+    });
+};
+
+module.exports = mongoose.model('user', userSchema);

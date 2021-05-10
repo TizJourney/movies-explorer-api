8a7b5c9decd6ae104d798e4f6824ab1103b47c94
Konstantin Bondar <kostya.bondar@gmail.com>
2021-05-10 16:35:20 +0300

    починить обращение к базе данных

--- app.js
+++ app.js
@@ -22,7 +22,7 @@ const { PORT = 3001 } = process.env;
 
 const app = express();
 
-mongoose.connect('mongodb://localhost:27017filmsdb', {
+mongoose.connect('mongodb://localhost:27017/filmsdb', {
   useNewUrlParser: true,
   useCreateIndex: true,
   useFindAndModify: false,
